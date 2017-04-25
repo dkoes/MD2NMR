8742969a982ee040fe9295f5c8dda111a7f46ce7
David Koes <dkoes@pitt.edu>
2017-04-24 23:17:24 -0400

    default to fixed filter constants

--- makedb.py
+++ makedb.py
@@ -7,7 +7,7 @@ import glob, re, collections
 import argparse, cPickle
 from scipy import spatial
 
-def make(tridir,keepoutliers=False):
+def make(tridir,keepoutliers=False,dynamicfilter=False):
     '''Create an optimized python database for looking up calculated chemical shifts by pattern'''
     db = dict()
     
@@ -58,24 +58,42 @@ def make(tridir,keepoutliers=False):
             for (pattern,vals) in valuesbypattern.iteritems():    
                 shifts = shiftsbypattern[pattern]         
                 if not keepoutliers:
-                    newshifts = []
-                    newvals = []
+                    if dynamicfilter:
+                        newshifts = []
+                        newvals = []
 
-                    for (nhc,v) in zip(shifts,vals):
-                        if atom == 'N':
-                            mean = nmean
-                            std = nstd
-                        else:
-                            mean = omean
-                            std = ostd
-                        for i in xrange(3):
-                            if nhc[i] < mean[i]-std[i]*3 or nhc[i] > mean[i]+std[i]*3:
-                                break
-                        else: #executed if we _didn't_ break out of the leep 
-                            newvals.append(v)
-                            newshifts.append(nhc)
-                    vals = newvals
-                    shifts = newshifts
+                        for (nhc,v) in zip(shifts,vals):
+                            if atom == 'N':
+                                mean = nmean
+                                std = nstd
+                            else:
+                                mean = omean
+                                std = ostd
+                            for i in xrange(3):
+                                if nhc[i] < mean[i]-std[i]*3 or nhc[i] > mean[i]+std[i]*3:
+                                    break
+                            else: #executed if we _didn't_ break out of the leep 
+                                newvals.append(v)
+                                newshifts.append(nhc)
+                        vals = newvals
+                        shifts = newshifts
+                    else: #not dynamic
+                        NMIN   = -10.936
+                        NMAX   = 7.559
+                        HMIN   = -2.966
+                        HMAX   = 0.195
+                        CMIN   = -3.159
+                        CMAX   = 5.5350
+                        newshifts = []
+                        newvals = []                        
+                        for (nhc,v) in zip(shifts,vals):
+                            if nhc[0] >= NMIN and nhc[0] <= NMAX and \
+                               nhc[1] >= HMIN and nhc[1] <= HMAX and \
+                               nhc[2] >= CMIN and nhc[2] <= CMAX:
+                                newvals.append(v)
+                                newshifts.append(nhc)
+                        vals = newvals
+                        shifts = newshifts
                 
                 if len(vals):
                     tree = spatial.cKDTree(np.array(vals),copy_data=True)
@@ -88,12 +106,13 @@ def make(tridir,keepoutliers=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d',"--tridir",help="directory containing .tri files")
+    parser.add_argument('-d',"--tridir",help="directory containing .tri files",required=True)
     parser.add_argument('-o',"--output",help="database output file",default="nmr.db")
     parser.add_argument('--disable-filter',help='do not remove outliers (3 sigma)',action='store_true')
+    parser.add_argument('--dynamic-filter',help='compute per-residue std and mean for filter, rather than using predefined constants',action='store_true')
     args = parser.parse_args()
     out = open(args.output,'w')
-    db = make(args.tridir,args.disable_filter)
+    db = make(args.tridir,args.disable_filter,args.dynamic_filter)
     #histidine variants
     db['HIS'] = db['HSE']
     db['HIE'] = db['HSE']
